1da81d52912ba3b43853d514a770738d67f74fc7
Jacob Perez <59049992+scjpleeds@users.noreply.github.com>
2023-10-31 16:23:07 +0000

    Update inputs.py

--- EDJO_finding_scripts/inputs.py
+++ EDJO_finding_scripts/inputs.py
@@ -1,11 +1,11 @@
 ### INPUTS FOR EDJO FINDING SCRIPT ### 
 
-PATH = '/Users/admin/OneDrive - University of Leeds/ARC4/scripts/edjo_diagnostics/' # path for where to save the files 
+PATH = ' ' # path for where to save the files 
 output_fname ='ERA5_19592020_diagnostics_Ucrit8' # two output files are made, <output_fname>_full.npz contains all data with no distinction between regions 
                   # <output_fname>_lm.npz contains the data of the regions with the largest umass on days with multiple objects
 
-var_cube_fname =  '/Users/admin/OneDrive - University of Leeds/ARC4/ERA/U19592020/U.nc' # Zonal wind cube file path 
-grid_cube_fname = '/Users/admin/OneDrive - University of Leeds/ARC4/scripts/gridarea.nc' # grid area cube file path 
+var_cube_fname =  'U.nc' # Zonal wind cube file path 
+grid_cube_fname = 'gridarea.nc' # grid area cube file path 
 
 
 cube_constraints = [(-60,0),(15,75),(850,850)] # Cube constraints, if already done with input cube enter as None 
