075e7002966e6b8cf8f3be77ff53ddd6473d0a26
Jacob Perez <59049992+scjpleeds@users.noreply.github.com>
2024-04-25 10:08:10 +0100

    Update edjo_diagnostic_finding_script.py
    fixed an issue with missing days

--- EDJO_finding_scripts/edjo_diagnostic_finding_script.py
+++ EDJO_finding_scripts/edjo_diagnostic_finding_script.py
@@ -3,8 +3,24 @@ import iris
 from functions import blob_finder,constrain_data,low_pass_filter
 from my_regions import EDJregion
 import warnings
-from inputs import * 
+# from inputs import * 
+import tqdm as tqdm
 warnings.filterwarnings('ignore')
+
+
+PATH = '/Users/admin/OneDrive - University of Leeds/ARC4/scripts/' # path for where to save the files 
+# two output files are made, <output_fname>_full.npz contains all data with no distinction between regions 
+                  # <output_fname>_lm.npz contains the data of the regions with the largest umass on days with multiple objects
+
+var_cube_fname =  '/Users/admin/OneDrive - University of Leeds/ARC4/ERA/U19592020/U.nc' # Zonal wind cube file path 
+grid_cube_fname = '/Users/admin/OneDrive - University of Leeds/ARC4/scripts/gridarea.nc' # grid area cube file path 
+
+
+cube_constraints = [(-60,0),(15,75),(850,850)] # Cube constraints, if already done with input cube enter as None 
+
+filtering = True # Apply a low pass Lanczos filter to the wind data 
+window, length = 61,10
+
 
 # Importing data
 var_cube =  iris.load_cube(var_cube_fname)
@@ -25,7 +41,18 @@ if filtering == True:
 if np.diff(pressure_constraint)!=0: 
     var_cube.collapsed('air_pressure',iris.analysis.MEAN)
 
+
+### Constraints for the EDJO finding algorithm 
+
+min_length = 1661
+min_zonal_length = 20
+flood_val = 8
 # Running EDJO finding algorithm 
+    
+
+
+
+output_fname ='ERA5_19592020_diagnostics_filtered' 
 regions_store,flood_store,region_maxima_coords= blob_finder(var_cube,flood_val,grid_cube,min_length,min_zonal_length)
 
 
@@ -37,8 +64,6 @@ N = len(var_cube.data)
 for i in range(0,N):
     num_of_labels.append(len(regions_store[i]))
 
-
-# 
 lon,lat = var_cube.coord('longitude').points,var_cube.coord('latitude').points
 
 
@@ -52,6 +77,8 @@ max_index_store = []
 com_y_full,com_x_full = [],[]
 orientation_values_full = []
 mean_intensity_full = [] 
+major_axis_length_full = []
+minor_axis_length_full = []
 area_full = []
 mass_full = []
 
@@ -61,13 +88,17 @@ for i in np.arange(0,N):
     com_x_store,com_y_store=[],[]
     orientation_values_store = []
     mean_intensity_store = []
-    
+    major_axis_length_store,minor_axis_length_store = [],[]
+
     if regions_store[i] == []:
         com_y_full.append([0])
         com_x_full.append([0])
-        mass_store.append([0])
+        mass_full.append([0])
         orientation_values_full.append([0])
-        area_store.append([0])
+        area_full.append([0])
+        mean_intensity_full.append([0])
+        major_axis_length_full.append([0])
+        minor_axis_length_full.append([0])
     else:
         for k,regions in enumerate(regions_store[i]):
             region_edj = EDJregion(regions[0],flood_store[i][k],var_cube.data[i],grid_cube.data,lon,lat)
@@ -76,6 +107,8 @@ for i in np.arange(0,N):
             mean_intensity_store.append(region_edj.mean_intensity())
             mass_store.append(region_edj.mass())
             area_store.append(region_edj.region_area())
+            major_axis_length_store.append(region_edj.get_axis_length())
+            minor_axis_length_store.append(region_edj.get_axis_width())
             orientation_values_store.append(region_edj.alpha())
 
         com_y_full.append(com_y_store)
@@ -85,11 +118,13 @@ for i in np.arange(0,N):
         area_full.append(area_store)
         mass_full.append(mass_store)
         max_mass_index = np.argmax(mass_store)
+        major_axis_length_full.append(major_axis_length_store)
+        minor_axis_length_full.append(minor_axis_length_store)
         max_index_store.append(max_mass_index)
-        
+    
         largest_mass_regions[i] = regions_store[i][max_mass_index]
         largest_mass_flood[i] = flood_store[i][max_mass_index]
-    
+
 
 com_y_full = np.array(com_y_full,dtype=object)
 com_x_full = np.array(com_x_full,dtype=object)     
@@ -97,6 +132,8 @@ orientation_values_full = np.array(orientation_values_full,dtype=object)
 mean_intensity_full = np.array(mean_intensity_full,dtype=object)
 area_full = np.array(area_full,dtype=object)
 mass_full = np.array(mass_full,dtype=object)
+major_axis_length_full = np.array(major_axis_length_full,dtype=object)
+minor_axis_length_full = np.array(minor_axis_length_full,dtype=object)
 
 
 
@@ -108,28 +145,33 @@ orientation_values = []
 com_y = []
 com_x = []
 mean_intensity = []
+major_axis_length_values,minor_axis_length_values = [],[]
 
 
 for i,region in enumerate(largest_mass_regions):
     if region == 0:
-       mass_values.append([0]) 
-       area_values.append([0])
-       orientation_values.append([0])
-       com_y.append([0])
-       com_x.append([0])
-       mean_intensity.append([0])
+        mass_values.append(0) 
+        area_values.append(0)
+        orientation_values.append(0)
+        com_y.append(0)
+        com_x.append(0)
+        mean_intensity.append(0)
+        major_axis_length_values.append(0)
+        minor_axis_length_values.append(0)
     else:
-        
+    
         region_edj = EDJregion(region[0],largest_mass_flood[i],var_cube.data[i],grid_cube.data,lon,lat)
         mass_values.append(region_edj.mass())
-        
+    
         com_y.append(region_edj.phibar())
         com_x.append(region_edj.lambdabar())
         orientation_values.append(region_edj.alpha())
-        
+    
         area_values.append(region_edj.region_area())
         mean_intensity.append(region_edj.mean_intensity())
-        
+        major_axis_length_values.append(region_edj.get_axis_length())
+        minor_axis_length_values.append(region_edj.get_axis_width())
+    
 
 mass_values = np.array(mass_values,dtype=object)
 area_values = np.array(area_values,dtype=object)
@@ -137,6 +179,8 @@ orientation_values = np.array(orientation_values,dtype=object)
 com_y = np.array(com_y,dtype=object)
 com_x = np.array(com_x,dtype=object)
 mean_intensity = np.array(mean_intensity,dtype=object)
+major_axis_length = np.array(major_axis_length_values,dtype=object)
+minor_axis_length = np.array(minor_axis_length_values,dtype=object)
 
-np.savez(output_fname+'_lm.npz',phibar=com_y,lambdabar=com_x,alpha=orientation_values,umean=mean_intensity,umass=mass_values,area=area_values,labels=num_of_labels,regions=largest_mass_regions,flood=largest_mass_flood)
-np.savez(output_fname+'_full.npz',phibar=com_y_full,lambdabar=com_x_full,alpha=orientation_values_full,umean=mean_intensity_full,umass=mass_full,labels=num_of_labels,regions=regions_store,flood=flood_store)
+np.savez(output_fname+'_lm.npz',phibar=com_y,lambdabar=com_x,alpha=orientation_values,umean=mean_intensity,umass=mass_values,area=area_values,major_axis_length=major_axis_length,minor_axis_length=minor_axis_length,labels=num_of_labels,regions=largest_mass_regions,flood=largest_mass_flood)
+np.savez(output_fname+'_full.npz',phibar=com_y_full,lambdabar=com_x_full,alpha=orientation_values_full,umean=mean_intensity_full,area=area_full,umass=mass_full,major_axis_length=major_axis_length_full,minor_axis_length=minor_axis_length_full,labels=num_of_labels,regions=regions_store,flood=flood_store)
